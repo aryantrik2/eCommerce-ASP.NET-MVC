d0593e51fe80d62bf89a159ec380d7e810a4569d
Aryan-PC <Aryan-PC@DESKTOP-A7107LL>
2023-04-06 16:20:25 +0530

    Added new Folders to the main Directory

---
+++ eCommerce/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace eCommerce.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext()
+        {
+            
+        }
+    }
+}
---
+++ eCommerce/Data/MovieCategory.cs
@@ -0,0 +1,10 @@
+﻿namespace eCommerce.Data
+{
+    public enum MovieCategory
+    {
+        Action=1,
+        Comedy,
+        Drama,
+        Documentary
+    }
+}
